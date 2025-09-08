1233bcf3300da2c25614e0bc51674c2eba31363c
Ramos Martínez Franklin David <140273170+Zhunhmaybe@users.noreply.github.com>
2025-09-08 16:02:53 -0500

    Conectar de program.cs a appsetting para la base de datos Creacion de DBContext

---
+++ Repaso.pagina/Pagina.Data/DBContext.cs
@@ -0,0 +1,15 @@
+﻿// Target framework for the code: .NET 8.0
+using Microsoft.EntityFrameworkCore;
+
+namespace Repaso.pagina.Pagina.Data
+{
+    public class DBContext: DbContext
+    {
+        //constructor // builder
+        public DBContext(DbContextOptions<DBContext> options ): base(options) 
+        {
+
+        }
+        // to do : add dbsets (tablas de la db)
+    }
+}
--- Repaso.pagina/Program.cs
+++ Repaso.pagina/Program.cs
@@ -1,6 +1,18 @@
+//1A)using to work with Entity Framework 
+using Microsoft.EntityFrameworkCore;//para hacer las conecciones de tipo sql server
+
 var builder = WebApplication.CreateBuilder(args);
 
+//2A)Coneccition with sql server express
+const string ConnectionName = "DefaultConnection";
+var conecctionString=builder.Configuration.GetConnectionString(ConnectionName) ?? throw new InvalidOperationException($"Connection string '{ConnectionName}' not found.");
+
+//3B)add DBContext to services
+
+
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
